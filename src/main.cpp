9df7605a44a0319c0524ad71fa557e2d0b6f8045
Xempt <Xemptuous>
2022-10-07 22:23:02 -0700

    added REPL functionality, and many header safeguards

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include "repl.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    system("cls");
+    string input;
+
+    cout << "Hello! Welcome to cimpl.\n";
+    cout << "Try typing some commands:\n";
+
+    while (true) {
+        cout << ">> ";
+        getline(cin, input);
+        if (input == "quit") {
+            return 0;
+        }
+        start(input);
+    }
+}
---
+++ src/repl.h
@@ -0,0 +1,14 @@
+#ifndef STRING
+#define STRING
+
+#include <string>
+
+#endif // STRING
+
+
+#ifndef START
+#define START
+
+void start(std::string);
+
+#endif // START
