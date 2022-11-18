2d6d3a8aa49096b9b1a5f12c288931938e14f347
Xemptuous <99044751+Xemptuous@users.noreply.github.com>
2022-11-17 18:05:20 -0800

    implemented basic scrolling pad with input

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,95 @@
-#include "object.hpp"
-#include <iostream>
+// #include "object.hpp"
+#include <ncurses.h>
+#include <string>
+#include <vector>
+#include <stack>
+// #include <iostream>
 
 using namespace std;
-void start(string, shared_ptr<Environment>);
+// void start(string, shared_ptr<Environment>);
 // void setEnvironment(shared_ptr<Environment> env);
-
+stack<string> CLI_STACK;
 
 int main() {
-  system("clear");
-  string input;
-  shared_ptr<Environment> env (new Environment);
+  initscr();
 
-  while (true) {
-start:
-    cout << ">> ";
-    getline(cin, input);
-    if (input == "quit") {
-      system("clear");
-      for (int i = 0; i < env->gc.size() - 1; i++) 
-        delete[] env->gc[i];
-      return 0;
+  int h{}, w{};
+  int padheight = 10000;
+  int padpos{0};
+  unsigned int cursor_x{4}, cursor_y{3};
+  getmaxyx(stdscr, h, w);
+
+
+  WINDOW* pad = newpad(LINES, COLS);
+  cbreak();
+  keypad(stdscr, true);
+  clearok(pad, true);
+  scrollok(pad, true);
+  idlok(pad, true);
+  setscrreg(h, w);
+
+  int ch;
+  while ((ch = mvgetch(cursor_y, cursor_x)) != KEY_EXIT) {
+    prefresh(pad, padpos, 0, 0, 0, LINES - 1, COLS - 1);
+    switch (ch) {
+      case KEY_UP:
+        if (cursor_y <= 1) {
+          cursor_y = 0;
+          padpos <= 1 ? padpos = 0 : padpos--;
+        }
+        else cursor_y--;
+        wmove(pad, cursor_y, cursor_x);
+        break;
+      case KEY_DOWN:
+        cursor_y >= h - 1 ? padpos++ : cursor_y++;
+        wmove(pad, cursor_y, cursor_x);
+        break;
+      case KEY_LEFT:
+        cursor_x <= 4 ? cursor_x = 4 : cursor_x--;
+        wmove(pad, cursor_y, cursor_x);
+        break;
+      case KEY_RIGHT:
+        cursor_x >= w - 2 ? cursor_x = w - 2 : cursor_x++;
+        wmove(pad, cursor_y, cursor_x);
+        break;
+      case 10:
+        wmove(pad, cursor_y, cursor_x);
+        cursor_y >= h - 1 ? cursor_y = h - 1 : cursor_y++;
+        cursor_x = 4;
+        wprintw(pad, "\n");
+        wprintw(pad, ">>> ");
+        prefresh(pad, padpos, 0, 0, 0, LINES - 1, COLS - 1);
+        break;
+      default:
+        pechochar(pad, ch);
+        cursor_x++;
+        break;
     }
-    if (input == "clear") {
-      system("clear");
-      goto start;
-    }
-    start(input, env);
   }
+  endwin();
+  delwin(pad);
+  clear();
+  refresh();
+  return 0;
+
+//   system("clear");
+//   string input;
+//   shared_ptr<Environment> env (new Environment);
+//
+//   while (true) {
+// start:
+//     std::cout << ">> ";
+//     std::getline(cin, input);
+//     if (input == "quit") {
+//       system("clear");
+//       for (int i = 0; i < env->gc.size() - 1; i++) 
+//         delete[] env->gc[i];
+//       return 0;
+//     }
+//     if (input == "clear") {
+//       system("clear");
+//       goto start;
+//     }
+//     start(input, env);
+//   }
 }
