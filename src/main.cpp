c478284f87588cdd8d6f39ef365a33703061ef0e
Xemptuous <99044751+Xemptuous@users.noreply.github.com>
2022-12-03 14:52:46 -0800

    added string reading of current line for repl purposes

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-// #include "object.hpp"
+#include "object.hpp"
 #include <ncurses.h>
 #include <string>
 #include <vector>
@@ -8,8 +8,6 @@
 using namespace std;
 // void start(string, shared_ptr<Environment>);
 // void setEnvironment(shared_ptr<Environment> env);
-string getstring();
-
 stack<string> CLI_STACK;
 
 int main() {
@@ -19,7 +17,7 @@ int main() {
   int padheight = 10000;
   int padpos{0};
   unsigned int cursor_x{4}, cursor_y{0};
-  unsigned int maxline_x{0};
+  unsigned int minx{4}, maxline_x{0};
   getmaxyx(stdscr, h, w);
 
   WINDOW* pad = newpad(LINES, COLS);
@@ -31,6 +29,7 @@ int main() {
   setscrreg(h, w);
 
   int ch;
+  // shared_ptr<Environment> env (new Environment);
   wprintw(pad, ">>> ");
   while ((ch = mvgetch(cursor_y, cursor_x)) != KEY_EXIT) {
     prefresh(pad, padpos, 0, 0, 0, LINES - 1, COLS - 1);
@@ -48,7 +47,7 @@ int main() {
         wmove(pad, cursor_y, cursor_x);
         break;
       case KEY_LEFT:
-        cursor_x <= 4 ? cursor_x = 4 : cursor_x--;
+        cursor_x <= minx ? cursor_x = minx : cursor_x--;
         wmove(pad, cursor_y, cursor_x);
         break;
       case KEY_RIGHT:
@@ -58,26 +57,36 @@ int main() {
       case KEY_BACKSPACE:
       case 127:
       case '\b':
-        cursor_x <= 4 ? cursor_x = 4 : cursor_x--;
+        cursor_x <= minx ? cursor_x = minx : cursor_x--;
         mvwdelch(pad, cursor_y, cursor_x);
         prefresh(pad, padpos, 0, 0, 0, LINES - 1, COLS - 1);
         maxline_x--;
         break;
       // enter key
-      case 10:
-        for(int i = 0; i < maxline_x; i++)
+      case 10: {
+        chtype p[150];
+        string input;
+        wmove(pad, cursor_y, minx);
+        winchnstr(pad, p, maxline_x);
+        for (int i = 0; i < sizeof(p) / sizeof(p[0]); i++) {
+          char y = p[i] & A_CHARTEXT;
+          input += y;
+        }
+        // moving to end of input before carriage return
+        for(int i = 0; i < maxline_x; i++) {
           wmove(pad, cursor_y, ++cursor_x);
+        }
+        // start(input, env);
         cursor_y >= h - 1 ? cursor_y = h - 1 : cursor_y++;
-        cursor_x = 4;
+        cursor_x = minx;
         wprintw(pad, "\n");
         wprintw(pad, ">>> ");
         prefresh(pad, padpos, 0, 0, 0, LINES - 1, COLS - 1);
         break;
+      }
       default:
-        // winsch(pad, ch);
         mvwinsch(pad, cursor_y, cursor_x, ch);
         prefresh(pad, padpos, 0, 0, 0, LINES - 1, COLS - 1);
-        // pechochar(pad, ch);
         maxline_x++;
         cursor_x++;
         break;
@@ -91,18 +100,6 @@ int main() {
 }
 
 
-string getstring() {
-  string input;
-  nocbreak();
-  echo();
-  int ch = getch();
-
-  while (ch != '\n') {
-    input += ch;
-    ch = getch();
-  }
-  return input;
-}
 //   system("clear");
 //   string input;
 //   shared_ptr<Environment> env (new Environment);
